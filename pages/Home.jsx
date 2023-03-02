5a91897f79af733aec6e216dfd3f5b35bc988d89
hirbo <hirbobalasa@gmail.com>
2023-03-02 11:58:33 +0300

    First Mobile Food delivery App

---
+++ pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View ,Text} from 'react-native'
+
+function Home() {
+  return (
+    <View className='p-32'>
+        <Text className='text-red-600'>homescreen</Text>
+    </View>
+  )
+}
+
+export default Home
