90ca129a7a1718460e6255b9b1af0619a1287f50
hirbo <hirbobalasa@gmail.com>
2023-03-02 13:36:00 +0300

    yy

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,22 +1,28 @@
-import React, { useLayoutEffect } from 'react'
-import { View ,Text} from 'react-native'
-import { useNavigation } from 'react-native-navigation'
-
+import React, { useLayoutEffect } from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import logo from "./logo.png";
 function Home() {
-const navigation = useNavigation
-    useLayoutEffect(()=>{
-        navigation.setOptions({
-            headerShown:false,
-        })
-    })
-
-
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
 
   return (
-    <View className='p-32'>
-        <Text className='text-red-600'>homescreen</Text>
+    <View className='mt-16 flex-row items-center'>
+      
+      <View >
+        <Image className="h-24 w-24" source={logo} />
+      </View>
+      <View className="text-red-600 flex-row">
+        <Text>Deliver Now</Text>
+        <Text>Current Loctaion</Text>
+      </View>
+      
     </View>
-  )
+  );
 }
 
-export default Home
+export default Home;
