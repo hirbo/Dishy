67380a1b0b921feebc169aa73bd0b0016abc0d41
hirbo <hirbobalasa@gmail.com>
2023-03-02 12:13:28 +0300

    dishy navigation

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { View ,Text} from 'react-native'
+import { useNavigation } from 'react-router-dom'
 
 function Home() {
+const navigation = useNavigation
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerShown:false,
+        })
+    })
+
+
+
   return (
     <View className='p-32'>
         <Text className='text-red-600'>homescreen</Text>
